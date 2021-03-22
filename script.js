7fafff56932e55617371374cf729ff4521bd20bf
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2021-03-22 17:10:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-var principal = 0;
-var rate = 0;
-var years = 0;
-var YEARR = 0;
-var Result = 0;
-function compute()
-{
-    var principal=document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var YEARR = 2020 + years;
-    var Interest = principal * years * rate / 100;
-    var Result = principal + Interest;
-    document.getElementsById("result").innerHTML = 
-    "If you deposit" + principal +
-    ",\n at an interest rate of " + rate + 
-    " %. \nYou will receive an amount of " 
-    + Result + ", \nin the year " + YEARR;       
-    
-}
 function Show_value(x){
     document.getElementById("Value").innerHTML = x;
 }
+function compute()
+{
+    
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    result = document.getElementById("result");
 
+    
+    Interest = principal * years * rate / 100;
 
+    
+    if (principal < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
 
+    
+    thisYear = new Date().getFullYear();
+    futureYear = thisYear + Number(years);
 
+    
+    result.innerHTML = "</br>If you deposit <mark>" + principal + "</mark>,</br> at an interest rate of <mark>" + rate + "</mark> %.</br> You will receive an amount of <mark>"  + Interest + "</br></mark> in the year <mark>" + futureYear + "</mark>.";
+}
+function Slider() {
+    
+    rate = document.getElementById("rate").value;
+    rateNumber = document.getElementById("Ratee");
 
-
+    
+    rateNumber.innerHTML = rate + " %";
+}
