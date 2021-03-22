e0d8d387b357b30c2e3192a3ca826df407630ebf
MuhammadSuleman97 <53903082+MuhammadSuleman97@users.noreply.github.com>
2021-03-22 16:10:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+var principal = 0;
+var rate = 0;
+var years = 0;
+var YEARR = 0;
+var Result = 0;
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal=document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var YEARR = 2020 + years;
+    var Interest = principal * years * rate / 100;
+    var Result = principal + Interest;
+    document.getElementsById("result").innerHTML = 
+    "If you deposit" + principal +
+    ",\n at an interest rate of " + rate + 
+    " %. \nYou will receive an amount of " 
+    + Result + ", \nin the year " + YEARR;       
     
 }
 function Show_value(x){
     document.getElementById("Value").innerHTML = x;
 }
-        
+
+
+
+
+
+
